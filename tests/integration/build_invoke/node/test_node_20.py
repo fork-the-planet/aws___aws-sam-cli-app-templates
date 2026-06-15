80d473d44a1bbf346f7f902d79fef40c4743a312
Erika Yao <71943596+erikayao93@users.noreply.github.com>
2026-06-15 09:34:00 -0700

    fix: address AL2 maven and nodejs20 CI failures (#551)

--- tests/integration/build_invoke/node/test_node_20.py
+++ tests/integration/build_invoke/node/test_node_20.py
@@ -9,49 +9,60 @@ For each template, it will test the following sam commands:
 
 class BuildInvoke_nodejs20_x_cookiecutter_aws_sam_hello_nodejs(BuildInvokeBase.SimpleHelloWorldBuildInvokeBase):
     directory = "nodejs20.x/hello"
+    should_test_lint = False
 
 class BuildInvoke_nodejs20_x_cookiecutter_aws_sam_step_functions_sample_app(BuildInvokeBase.BuildInvokeBase):
     directory = "nodejs20.x/step-func"
+    should_test_lint = False
 
 
 class BuildInvoke_nodejs20_x_cookiecutter_quick_start_from_scratch(BuildInvokeBase.BuildInvokeBase):
     directory = "nodejs20.x/scratch"
+    should_test_lint = False
 
 
 class BuildInvoke_nodejs20_x_cookiecutter_quick_start_cloudwatch_events(BuildInvokeBase.BuildInvokeBase):
     directory = "nodejs20.x/cw-event"
+    should_test_lint = False
 
 
 class BuildInvoke_nodejs20_x_cookiecutter_quick_start_s3(BuildInvokeBase.BuildInvokeBase):
     directory = "nodejs20.x/s3"
+    should_test_lint = False
 
     # if we want to check response json, we need to setup bucket for testing
 
 
 class BuildInvoke_nodejs20_x_cookiecutter_quick_start_sns(BuildInvokeBase.BuildInvokeBase):
     directory = "nodejs20.x/sns"
+    should_test_lint = False
 
 
 class BuildInvoke_nodejs20_x_cookiecutter_quick_start_sqs(BuildInvokeBase.BuildInvokeBase):
     directory = "nodejs20.x/sqs"
+    should_test_lint = False
 
 
 class BuildInvoke_nodejs20_x_cookiecutter_quick_start_response_streaming(BuildInvokeBase.BuildInvokeBase):
     directory = "nodejs20.x/response-streaming"
+    should_test_lint = False
 
 
 class BuildInvoke_nodejs20_x_cookiecutter_quick_start_web(BuildInvokeBase.QuickStartWebBuildInvokeBase):
     directory = "nodejs20.x/web"
+    should_test_lint = False
 
 
 class BuildInvoke_nodejs20_x_cookiecutter_quick_start_full_stack(BuildInvokeBase.QuickStartWebBuildInvokeBase):
     directory = "nodejs20.x/full-stack"
+    should_test_lint = False
 
 
 class BuildInvoke_image_nodejs20_x_cookiecutter_aws_sam_hello_nodejs_lambda_image(
     BuildInvokeBase.SimpleHelloWorldBuildInvokeBase
 ):
     directory = "nodejs20.x/hello-img"
+    should_test_lint = False
 
 
 class BuildInvoke_nodejs20_x_cookiecutter_aws_sam_gql_quick_start(BuildInvokeBase.BuildInvokeBase):
--- tests/integration/unit_test/test_unit_test_nodejs20_x.py
+++ tests/integration/unit_test/test_unit_test_nodejs20_x.py
@@ -4,6 +4,7 @@ from tests.integration.unit_test.unit_test_base import UnitTestBase
 class UnitTest_nodejs20_x_cookiecutter_aws_sam_hello_nodejs(UnitTestBase.NodejsUnitTestBase):
     directory = "nodejs20.x/hello"
     code_directories = ["hello-world"]
+    should_test_lint = False
 
 
 class UnitTest_nodejs20_x_cookiecutter_aws_sam_step_functions_sample_app(UnitTestBase.NodejsUnitTestBase):
@@ -13,40 +14,49 @@ class UnitTest_nodejs20_x_cookiecutter_aws_sam_step_functions_sample_app(UnitTes
         "functions/stock-checker",
         "functions/stock-seller",
     ]
+    should_test_lint = False
 
 
 class UnitTest_nodejs20_x_cookiecutter_quick_start_from_scratch(UnitTestBase.NodejsUnitTestBase):
     directory = "nodejs20.x/scratch"
+    should_test_lint = False
 
 
 class UnitTest_nodejs20_x_cookiecutter_quick_start_cloudwatch_events(UnitTestBase.NodejsUnitTestBase):
     directory = "nodejs20.x/cw-event"
+    should_test_lint = False
 
 
 class UnitTest_nodejs20_x_cookiecutter_quick_start_response_streaming(UnitTestBase.NodejsUnitTestBase):
     directory = "nodejs20.x/response-streaming"
     code_directories = ["src"]
+    should_test_lint = False
 
 
 class UnitTest_nodejs20_x_cookiecutter_quick_start_s3(UnitTestBase.NodejsUnitTestBase):
     directory = "nodejs20.x/s3"
+    should_test_lint = False
 
 
 class UnitTest_nodejs20_x_cookiecutter_quick_start_sns(UnitTestBase.NodejsUnitTestBase):
     directory = "nodejs20.x/sns"
+    should_test_lint = False
 
 
 class UnitTest_nodejs20_x_cookiecutter_quick_start_sqs(UnitTestBase.NodejsUnitTestBase):
     directory = "nodejs20.x/sqs"
+    should_test_lint = False
 
 
 class UnitTest_nodejs20_x_cookiecutter_quick_start_web(UnitTestBase.NodejsUnitTestBase):
     directory = "nodejs20.x/web"
+    should_test_lint = False
 
 
 class UnitTest_nodejs20_x_cookiecutter_quick_start_full_stack(UnitTestBase.NodejsUnitTestBase):
     directory = "nodejs20.x/full-stack"
     code_directories = ["backend", "frontend"]
+    should_test_lint = False
 
 
 class UnitTest_nodejs20_x_cookiecutter_aws_sam_gql_quick_start(UnitTestBase.NodejsUnitTestBase):
